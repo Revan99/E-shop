43276688f5afad2585718dc8de52a66cb09bcd11
Revan Sarbast Salih <revansarbast8@gmail.com>
2022-01-08 09:00:00 +0300

    fix create card

---
+++ src/component/card/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ className, imageSrc, text }) => {
+  return (
+    <Link
+      to={`/${text}`}
+      className={`flex flex-col max-w-full mx-2 md:mx-0 rounded-xl border border-secondary-2 shadow-2xl overflow-hidden ${className}`}
+    >
+      <div className="flex-1 flex h-2/3">
+        <img src={imageSrc} alt={text} className="w-full h-full" />
+      </div>
+      <h1 className="text-primary-1 text-3xl text-center my-10">{text}</h1>
+    </Link>
+  );
+};
+
+export default Card;
---
+++ src/pages/Text.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Card from "../component/card/Card";
+
+const Text = () => {
+  const stores = useSelector((state) => state.stores);
+  const [search, setSearch] = useState("");
+  console.log(stores);
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className="w-full h-full bg-secondary-1 p-10 flex flex-col">
+      <div className=" mb-10  p-1">
+        <input
+          type="text"
+          className="w-full h-12 rounded-xl  bg-secondary-2 text-white text-xl pl-4"
+          value={search}
+          placeholder="Search for stores"
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="grid gap-10  grid-cols-4 w-full h-full overflow-auto">
+        {stores.map((store) => {
+          if (!store.name.toLowerCase().includes(search.toLowerCase())) return;
+          return (
+            <Card imageSrc={store.logo} text={store.name} key={store.id} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Text;
